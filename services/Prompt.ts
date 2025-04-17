749284a4af4bf1df4ce108e84884f9e5bb2a2e9a
Abhik Baidya <abhikbaidya7@gmail.com>
2025-04-17 19:47:37 +0530

    Category list prompt added

--- services/Prompt.ts
+++ services/Prompt.ts
@@ -3,7 +3,7 @@ const GENERATE_RECIPE_OPTION_PROMPT =
 
 const GENERATE_COMPLETE_RECIPE_PROMPT =
   "- As per recipe Name and Description, give me all the ingredients as `ingredient` and give the emoji icons for each ingredients as `ingredient`,give the each quantity as `quantity`, and give the detailed step-by-step instructions for make this as `steps`. " +
-  "Also give the total calories of this recipe as `calories` (only number),give the take time to cook as `cookTime`,give the number of servings of this recipe as `serveTo`, and give a realistic image text prompt as `imagePrompt`. " +
+  "Also give the total calories of this recipe as `calories` (only number),give the take time to cook as `cookTime`,give the number of servings of this recipe as `serveTo`, and give a realistic image text prompt as `imagePrompt`. And also Give me category List for recipe from[Breakfast,Lunch,Dinner,Salad,Dessert,Fastfood,Drink,Cake] as category" +
   "Give me the response in JSON format only.";
 
 export { GENERATE_RECIPE_OPTION_PROMPT, GENERATE_COMPLETE_RECIPE_PROMPT };
