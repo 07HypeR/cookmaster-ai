a5564eecc4d19a7239a44866e636480c775e13e0
Abhik Baidya <abhikbaidya7@gmail.com>
2025-04-19 01:47:15 +0530

    Explore page added

--- app/(tabs)/Explore.tsx
+++ app/(tabs)/Explore.tsx
@@ -1,10 +1,50 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import Colors from "@/services/Colors";
+import GlobalApi from "@/services/GlobalApi";
+import RecipeCard from "@/components/RecipeCard";
 
 const Explore = () => {
+  const [recipeList, setRecipeList] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    GetAllRecipes();
+  }, []);
+
+  const GetAllRecipes = async () => {
+    const result = await GlobalApi.GetAllRecipeList();
+    console.log(result.data.data);
+    setRecipeList(result.data.data);
+  };
   return (
-    <View>
-      <Text>Explore</Text>
+    <View
+      style={{
+        padding: 20,
+        backgroundColor: Colors.WHITE,
+        height: "100%",
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 30,
+        }}
+      >
+        Explore
+      </Text>
+      <FlatList
+        data={recipeList}
+        numColumns={2}
+        refreshing={loading}
+        onRefresh={GetAllRecipes}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item, index }) => (
+          <View style={{ flex: 1 }}>
+            <RecipeCard recipe={item} />
+          </View>
+        )}
+      />
     </View>
   );
 };
