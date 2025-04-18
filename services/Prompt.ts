b841f78ef33bed07543cb5d7b8b6395dbcc2ba91
Abhik Baidya <abhikbaidya7@gmail.com>
2025-04-18 16:31:16 +0530

    Prompt changes

--- services/Prompt.ts
+++ services/Prompt.ts
@@ -1,9 +1,19 @@
-const GENERATE_RECIPE_OPTION_PROMPT =
-  ": Depends on user instruction, create 3 different Recipe variants with Recipe Name (with Emoji), a 2-line description, and a main ingredient list in JSON format with fields: recipeName, description, ingredients (without size) only.";
+export default {
+  GENERATE_RECIPE_OPTION_PROMPT: `:Depends on user instruction create 3 different Recipe variant with Recipe Name with Emoji, 
 
-const GENERATE_COMPLETE_RECIPE_PROMPT =
-  "- As per recipe Name and Description, give me all the ingredients as `ingredient` and give the emoji icons for each ingredients as `ingredient`,give the each quantity as `quantity`, and give the detailed step-by-step instructions for make this as `steps`. " +
-  "Also give the total calories of this recipe as `calories` (only number),give the take time to cook as `cookTime`,give the number of servings of this recipe as `serveTo`, and give a realistic image text prompt as `imagePrompt`. And also Give me category List for recipe from[Breakfast,Lunch,Dinner,Salad,Dessert,Fastfood,Drink,Cake] as category" +
-  "Give me the response in JSON format only.";
+2 line description and main ingredient list in JSON format with field recipeName,description,ingredients (without size) only`,
 
-export { GENERATE_RECIPE_OPTION_PROMPT, GENERATE_COMPLETE_RECIPE_PROMPT };
+  GENERATE_COMPLETE_RECIPE_PROMPT: ` 
+
+        - As per recipe Name and Description, Give me all list of ingredients as ingredient ,
+
+        - emoji icons for each ingredient as icon, quantity as quantity, along with detail step by step  recipe as steps
+
+        - Total Calories as calories (only number), Minutes to cook as cookTime and serving number as serveTo
+
+        - relastic image Text prompt as per reciepe as imagePrompt
+
+        -Give me category List for recipe from[Breakfast,Lunch,Dinner,Salad,Dessert,Fastfood,Drink,Cake] as category"
+
+        - Give me response in JSON format only`,
+};
