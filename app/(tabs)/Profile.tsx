bc752cd1dbd0524ea13bbbd7ebc010524ba0d144
Abhik Baidya <abhikbaidya7@gmail.com>
2025-04-19 15:37:22 +0530

    Profile Screen added

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,14 +1,139 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  FlatList,
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useContext } from "react";
+import Colors from "@/services/Colors";
+import { UserContext } from "@/context/UserContext";
+import { useRouter } from "expo-router";
+import { useLogto } from "@logto/rn";
 
 const Profile = () => {
+  const { user } = useContext(UserContext);
+  const { signOut } = useLogto();
+  const option = [
+    {
+      name: "Create New Recipe",
+      icon: require("./../../assets/images/i1.png"),
+      path: "/(tabs)/Home",
+    },
+    {
+      name: "My Recipes",
+      icon: require("./../../assets/images/i3.png"),
+      path: "/(tabs)/Cookbook",
+    },
+    {
+      name: "Browse More Recipes",
+      icon: require("./../../assets/images/i2.png"),
+      path: "/(tabs)/Explore",
+    },
+    {
+      name: "Logout",
+      icon: require("./../../assets/images/power.png"),
+      path: "logout",
+    },
+  ];
+  const router = useRouter();
+  const onOptionClick = async (option: any) => {
+    if (option.path === "logout") {
+      await signOut();
+      router.replace("/Landing");
+      return;
+    }
+    router.push(option.path);
+  };
   return (
-    <View>
-      <Text>Profile</Text>
+    <View
+      style={{
+        height: "100%",
+        backgroundColor: Colors.WHITE,
+        padding: 20,
+      }}
+    >
+      <View
+        style={{
+          ...(Platform.OS === "ios" ? { marginVertical: 30 } : {}),
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "outfit-bold",
+            fontSize: 30,
+          }}
+        >
+          Profile
+        </Text>
+        <View
+          style={{
+            display: "flex",
+            alignItems: "center",
+            marginTop: 20,
+          }}
+        >
+          <Image
+            source={{ uri: user?.picture }}
+            style={{
+              width: 80,
+              height: 80,
+              borderRadius: 99,
+            }}
+          />
+          <Text
+            style={{
+              fontFamily: "outfit-bold",
+              fontSize: 25,
+              marginTop: 20,
+            }}
+          >
+            {user?.name}
+          </Text>
+          <Text
+            style={{
+              fontFamily: "outfit",
+              fontSize: 17,
+              color: Colors.GRAY,
+            }}
+          >
+            {user?.email}
+          </Text>
+        </View>
+
+        <FlatList
+          data={option}
+          style={{
+            marginTop: 25,
+          }}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity
+              onPress={() => onOptionClick(item)}
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 7,
+                padding: 20,
+              }}
+            >
+              <Image source={item.icon} style={{ width: 40, height: 40 }} />
+              <Text
+                style={{
+                  fontFamily: "outfit",
+                  fontSize: 20,
+                }}
+              >
+                {item.name}
+              </Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
     </View>
   );
 };
 
 export default Profile;
-
-const styles = StyleSheet.create({});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,6 +31,12 @@ const RootLayout = () => {
             }}
           />
           <Stack.Screen
+            name="index"
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
             name="(tabs)"
             options={{
               headerShown: false,
