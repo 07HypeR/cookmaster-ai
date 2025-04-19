e9df73c9631bc17091962039800e0806cc50443a
Abhik Baidya <abhikbaidya7@gmail.com>
2025-04-19 11:52:17 +0530

    CookBook screen added

--- app/(tabs)/Cookbook.tsx
+++ app/(tabs)/Cookbook.tsx
@@ -1,10 +1,53 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import Colors from "@/services/Colors";
+import GlobalApi from "@/services/GlobalApi";
+import { UserContext } from "@/context/UserContext";
+import RecipeCard from "@/components/RecipeCard";
 
 const Cookbook = () => {
+  const { user } = useContext(UserContext);
+  const [recipeList, setRecipeList] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    GetUserRecipeList();
+  }, []);
+
+  const GetUserRecipeList = async () => {
+    const result = await GlobalApi.GetUserCreatedRecipe(user?.email);
+    console.log(result.data.data);
+    setRecipeList(result.data.data);
+  };
+
   return (
-    <View>
-      <Text>Cookbook</Text>
+    <View
+      style={{
+        padding: 20,
+        backgroundColor: Colors.WHITE,
+        height: "100%",
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 35,
+        }}
+      >
+        Cookbook
+      </Text>
+      <FlatList
+        data={recipeList}
+        numColumns={2}
+        refreshing={loading}
+        onRefresh={GetUserRecipeList}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item, index }) => (
+          <View style={{ flex: 1 }}>
+            <RecipeCard recipe={item} />
+          </View>
+        )}
+      />
     </View>
   );
 };
